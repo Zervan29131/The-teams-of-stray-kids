d25b4b034628c354b1380331518c5f54ee7e8ff0
zervan <nickmechlin01@outlook.com>
2022-09-16 00:25:33 +0800

    addnew

---
+++ cloudfunctions/updateCat/index.js
@@ -0,0 +1,52 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({env: cloud.DYNAMIC_CURRENT_ENV})
+
+function deepcopy(origin) {
+  // not for modifying.
+  const copyKeys = ['area', 'campus', 'characteristics',
+    'colour', 'father', 'gender', 'mother', 'name', 'nickname', 'popularity', 'sterilized', 'adopt',
+    'birthday', 'habit', 'tutorial', 'relation', 'to_star']
+  var res = {};
+  for (const key of copyKeys) {
+    res[key] = origin[key];
+  }
+  return res;
+}
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  if (event.deploy_test === true) {
+    // 进行部署检查
+    return;
+  }
+  const wxContext = cloud.getWXContext();
+  const openid = wxContext.OPENID;
+  const isManager = (await cloud.callFunction({ name: 'isManager', data: { openid: openid, req: 2 }}));
+  if (!isManager.result) {
+    return { msg: 'not a manager', result: isManager };
+  }
+
+  console.log(event);
+  var cat = event.cat;
+  const cat_id = event.cat_id;
+
+  const db = cloud.database();
+  if (cat_id) {
+    // 是更新
+    console.log("开始更新：" + cat_id);
+    var cat_data = deepcopy(cat);
+    return db.collection('cat').doc(cat_id).update({
+      data: cat_data
+    })
+  } else {
+    // 是新猫
+    console.log("开始添加新猫");
+    const count = (await db.collection('cat').count()).total;
+    cat._no = 'c' + count;
+    return db.collection('cat').add({
+      data: cat
+    })
+  }
+}
